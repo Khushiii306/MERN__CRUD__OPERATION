a2689f35d7bf7186ffeb471ef61bc99617a553e1
khushbu <sonawanekhushbu@gmail.com>
2022-01-04 01:39:51 +0530

    MERN__CRUD__OPERATION

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import Login from './Login';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Registration from './Registration';
+import  Dashboard  from './Dashboard';
 
 function App() {
+  const fetchUsers = () =>{
+    axios.get("/api/list").then((res) => console.log(res.data));
+  };
+
+const user = {
+  username:"",
+  password:"",
+  name:"",
+  age:0,
+};
+
+const registerUsers = () => {
+  axios.post("/api/registration", user).then(res => console.log(res.data));
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/registration" element={<Registration />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        </Routes>
+                        </BrowserRouter>
+     
     </div>
   );
 }
